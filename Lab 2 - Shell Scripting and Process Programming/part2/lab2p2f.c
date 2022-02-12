07b31ffc9a466029dc5afc20e6d7a07a700648af
Wong Chee Hong <c.h.wong2606@gmail.com>
2022-02-13 03:00:55 +0800

    lab 2 part 2 done

--- Lab 2 - Shell Scripting and Process Programming/part2/lab2p2f.c
+++ Lab 2 - Shell Scripting and Process Programming/part2/lab2p2f.c
@@ -16,5 +16,33 @@ int main() {
     // WITHOUT using | and > from the shell.
     //
 
+    int p[2];
+
+    if (pipe(p) < 2)
+        perror("lab2p2f: ");
+
+    if (fork() != 0) { //parent
+        char buffer[4096];
+        close(p[1]); //not writing
+        wait(NULL); //wait for child to finish
+
+        int fp_out = open("./result.out", O_CREAT | O_WRONLY);
+        dup2(p[0], STDIN_FILENO);
+        dup2(fp_out, STDOUT_FILENO);
+        execlp("./talk", "talk", (char *) 0);
+        close(p[0]);
+        close(fp_out);
+
+    } else {
+        close(p[0]); //not reading
+        dup2(p[1], STDOUT_FILENO); //open p[1] on stdout
+        close(p[1]);  //close p[1]
+        execlp("./slow", "slow", "5", NULL);
+        close(STDOUT_FILENO); //close pipe
+
+    }
+
+
+
 }
 
